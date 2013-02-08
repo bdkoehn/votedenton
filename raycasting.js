72dd57a697104f6cc0970cb883c9254cf3b6b8c2
David Myers <david.d.myers@gmail.com>
2013-02-07 23:03:58 -0600

    Added raycasting js to main script

--- raycasting.js
+++ raycasting.js
@@ -50,11 +50,3 @@ google.maps.Polygon.prototype.Contains = function(point) {
 	}
 
 };
-
-var point = new google.maps.LatLng(52.05249047600099, -0.6097412109375);
-var polygon = new google.maps.Polygon({
-	path : [INSERT_PATH_ARRAY_HERE]
-});
-if (polygon.Contains(point)) {
-	// point is inside polygon
-}
