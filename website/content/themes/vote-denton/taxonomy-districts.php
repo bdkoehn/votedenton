e4eeaaf09a9615ffe43990b98749240fb167c9e0
Patrick Daly <patrick@developdaly.com>
2013-02-25 21:59:00 -0600

    Added templates for candidates and districts

---
+++ website/content/themes/vote-denton/taxonomy-districts.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Archive Template
+ *
+ * The archive template is the default template used for archives pages without a more specific template. 
+ *
+ * @package Vote Denton
+ * @subpackage Template
+ */
+
+get_header(); // Loads the header.php template. ?>
+
+	<?php do_atomic( 'before_content' ); // vote_denton_before_content ?>
+
+	<div id="content" class="<?php echo vote_denton_get_layout( 'content' ); ?>">
+
+		<?php do_atomic( 'open_content' ); // vote_denton_open_content ?>
+
+		<div class="hfeed">
+			
+			<div class="container">
+
+				<?php get_template_part( 'loop-meta' ); // Loads the loop-meta.php template. ?>
+			
+			</div>
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php do_atomic( 'before_entry' ); // vote_denton_before_entry ?>
+
+					<div id="post-<?php the_ID(); ?>" class="<?php hybrid_entry_class(); ?>">
+						
+						<div class="container">
+
+							<?php do_atomic( 'open_entry' ); // vote_denton_open_entry ?>
+	
+							<?php if ( current_theme_supports( 'get-the-image' ) ) get_the_image( array( 'meta_key' => 'Thumbnail', 'size' => 'thumbnail' ) ); ?>
+	
+							<?php echo apply_atomic_shortcode( 'entry_title', '[entry-title]' ); ?>
+	
+							<?php echo apply_atomic_shortcode( 'byline', '<div class="byline">' . __( 'By [entry-author] on [entry-published]', 'enterprise' ) . '</div>' ); ?>
+	
+							<div class="entry-summary">
+								<?php the_excerpt(); ?>
+								<?php wp_link_pages( array( 'before' => '<p class="page-links">' . __( 'Pages:', 'enterprise' ), 'after' => '</p>' ) ); ?>
+							</div><!-- .entry-summary -->
+	
+							<?php echo apply_atomic_shortcode( 'entry_meta', '<div class="entry-meta">' . __( '[entry-terms taxonomy="category" before="Posted in "] [entry-terms before="| Tagged "] [entry-comments-link before=" | "]', 'enterprise' ) . '</div>' ); ?>
+	
+							<?php do_atomic( 'close_entry' ); // vote_denton_close_entry ?>
+						
+						</div><!-- .container -->
+
+					</div><!-- .hentry -->
+
+					<?php do_atomic( 'after_entry' ); // vote_denton_after_entry ?>
+
+				<?php endwhile; ?>
+
+			<?php else : ?>
+
+				<?php get_template_part( 'loop-error' ); // Loads the loop-error.php template. ?>
+
+			<?php endif; ?>
+
+		</div><!-- .hfeed -->
+
+		<?php do_atomic( 'close_content' ); // vote_denton_close_content ?>
+
+		<?php get_template_part( 'loop-nav' ); // Loads the loop-nav.php template. ?>
+
+	</div><!-- #content -->
+
+	<?php do_atomic( 'after_content' ); // vote_denton_after_content ?>
+
+<?php get_footer(); // Loads the footer.php template. ?>
