99fd970638c8642eb5c11294286439ced2fb77ac
David Myers <david.d.myers@gmail.com>
2013-02-27 15:55:46 -0600

    Fixed IE<9 JS compatibility issue

--- website/content/themes/vote-denton/js/plugins.js
+++ website/content/themes/vote-denton/js/plugins.js
@@ -1,3 +1,27 @@
+// Add indexOf function to IE < 9
+if (!Array.prototype.indexOf)
+{
+  Array.prototype.indexOf = function(elt /*, from*/)
+  {
+    var len = this.length >>> 0;
+
+    var from = Number(arguments[1]) || 0;
+    from = (from < 0)
+         ? Math.ceil(from)
+         : Math.floor(from);
+    if (from < 0)
+      from += len;
+
+    for (; from < len; from++)
+    {
+      if (from in this &&
+          this[from] === elt)
+        return from;
+    }
+    return -1;
+  };
+}
+
 // Avoid `console` errors in browsers that lack a console.
 (function() {
     var method;
